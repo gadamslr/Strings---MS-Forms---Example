7e9bf56bb6a42542b0bfe61e7251a3607f5c499a
Gareth Adams <gadams@longroad.ac.uk>
2022-12-01 14:45:40 +0000

    Add project files.

---
+++ Strings - MS Forms quiz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings___MS_Forms_quiz
+{
+    internal class Program
+    {
+        static string Replace(string S1, string S2, string S3)
+        {
+            int S = S1.IndexOf(S2);
+
+            int E = S + S2.Length;
+
+            string NewString = S1.Substring(0, S);
+
+            NewString += S3;
+
+            NewString += S1.Substring(E, S1.Length - E);
+
+            return NewString;
+        }
+
+        static void Main(string[] args)
+        {
+         
+            string Phrase = "Strings are the root of physics.";
+
+            string Word1 = "are";
+
+
+            string Word2 = "may be";
+            
+            Console.WriteLine(Replace(Phrase, Word1, Word2));
+
+        }
+    }
+}
